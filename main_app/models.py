8b4191c9610e5f61f860edab44fbd4ff69ceef00
onias17 <oniasnephiisrael@gmail.com>
2021-05-25 16:43:13 -0700

    message model

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+
+class Message(models.Model):
+    name = models.CharField(max_length=100)
+    phone = models.CharField(
+        'Phone Number',
+        max_length=13
+    )
+    email = models.EmailField(max_length=100)
+    message = models.TextField(
+        'Let us know what you need!',
+        max_length=500
+    )
+    date_created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-id']
+
+    def __str__(self):
+        return self.email
