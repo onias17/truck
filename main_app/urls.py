ec9cdbe55567bd9a22925a7835bb568da9c091bf
onias17 <oniasnephiisrael@gmail.com>
2021-05-25 21:13:04 -0700

    success message url

--- main_app/urls.py
+++ main_app/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.home, name='home'),
     # Message Routes
     path('messages/new/', views.add_message, name='add_message'),
+    path('messages/success/', views.message_success, name='success'),
 ]
