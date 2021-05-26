5e73c3efb55e02f75c315ae33f25ff7ed1c441c7
onias17 <oniasnephiisrael@gmail.com>
2021-05-26 11:43:38 -0700

    settings

--- truck/settings.py
+++ truck/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 import os
-# import django_on_heroku
+import django_on_heroku
 
 from pathlib import Path
 
@@ -150,4 +150,4 @@ ADMINS = [
 ]
 
 
-# django_on_heroku.settings(locals())
+django_on_heroku.settings(locals())
