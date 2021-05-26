65f05ce2d24136a4d966f28a6d31825f1a7d3b54
onias17 <oniasnephiisrael@gmail.com>
2021-05-25 22:14:03 -0700

    email views setup

--- main_app/views.py
+++ main_app/views.py
@@ -16,16 +16,38 @@ def add_message(request):
         if message_form.is_valid():
             new_message = message_form.save(commit=False)
             new_message.save()
-            # message = loader.render_to_string(
-            #     'emails/admin1.html',
-            #     {
-            #         'name': new_message.name,
-            #         'phone_number': new_message.phone,
-            #         'email': new_message.email,
-            #         'message': new_message.message,
-            #         'date_created': new_message.date_created,
-            #     }
-            # )
+            message = loader.render_to_string(
+                'emails/admin.html',
+                {
+                    'name': new_message.name,
+                    'phone': new_message.phone,
+                    'email': new_message.email,
+                    'message': new_message.message,
+                    'date_created': new_message.date_created,
+                }
+            )
+            mail_admins(
+                "New Message",
+                "A new message has been received.",
+                fail_silently=False,
+                connection=None,
+                html_message=message,
+            )
+            html_message = loader.render_to_string(
+                'emails/client.html',
+                {
+                    'subject': 'We have received your message!',
+                    'messsage': new_message.messsage,
+                }
+            )
+            send_mail(
+                "Message",
+                "We have received your message! ",
+                "austin.wilson@gadditeexpressllc.com",
+                [new_message.email],
+                fail_silently=False,
+                html_message=html_message
+            )
             return render(request, 'messages/success.html')
     else:
         form = MessageForm()
