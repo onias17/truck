eed5124232e2d3e9bdc002293f37344705c7f841
onias17 <oniasnephiisrael@gmail.com>
2021-05-26 11:36:50 -0700

    heroku

--- truck/settings.py
+++ truck/settings.py
@@ -127,6 +127,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'main_app/static'),
+)
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+MEDIA_URL = '/media/'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.ionos.com'
